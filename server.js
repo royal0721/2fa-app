507a094d3d0b5aaa68a1b8f59b93788b91372172
Clemmy Liao <royal0721@gmail.com>
2025-03-24 20:05:18 +0800

    add client

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+// 指定靜態檔路徑（Angular build 出來的資料夾）
+app.use(express.static(path.join(__dirname, "dist/2fa-client")));
+
+// 將其他未指定的路由交由 Angular 處理
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/2fa-client"));
+});
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/api", authRoutes);
